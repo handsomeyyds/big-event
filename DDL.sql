bf8d7785b152a84f67cab754996380bfe38ae752
Zhengyy <handsomeyyds@aliyun.com>
2024-03-12 16:55:14 +0800

    sql

--- DDL.sql
+++ DDL.sql
@@ -1,31 +1,119 @@
-create table big_event.user (
-                                id int unsigned primary key auto_increment comment 'ID',
-                                username varchar(20) not null unique comment '用户名',
-                                password varchar(32) comment '密码',
-                                nickname varchar(10) default '' comment '昵称',
-                                email varchar(128) default '' comment '邮箱',
-                                user_pic varchar(128) default '' comment '头像',
-                                create_time datetime not null comment '创建时间',
-                                update_time datetime not null comment '修改时间'
-) comment '用户表';
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: localhost    Database: big_event
+-- ------------------------------------------------------
+-- Server version	8.3.0
 
-create table big_event.category(
-                                   id int unsigned primary key auto_increment comment 'ID',
-                                   category_name varchar(32) not null comment '分类名词',
-                                   category_alias varchar(32) not null comment '分类别名',
-                                   create_user int unsigned not null comment '创建人ID',
-                                   create_time datetime not null comment '创建时间',
-                                   update_time datetime not null comment '修改时间'
-) comment '分类表';
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-create table big_event.article(
-                                  id int unsigned primary key auto_increment comment 'ID',
-                                  title varchar(30) not null comment '标题',
-                                  cover_img varchar(128) not null comment '封面',
-                                  state varchar(3) default '草稿' comment '状态，已发布或草稿',
-                                  category_id int unsigned comment '分类ID',
-                                  create_user int unsigned not null comment '作者',
-                                  content text not null comment '内容',
-                                  create_time datetime not null comment '创建时间',
-                                  update_time datetime not null comment '更新时间'
-) comment '文章表';
+--
+-- Table structure for table `article`
+--
+
+DROP TABLE IF EXISTS `article`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `article` (
+                           `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
+                           `title` varchar(30) NOT NULL COMMENT '标题',
+                           `cover_img` varchar(128) NOT NULL COMMENT '封面',
+                           `state` varchar(3) DEFAULT '草稿' COMMENT '状态，已发布或草稿',
+                           `category_id` int unsigned DEFAULT NULL COMMENT '分类ID',
+                           `create_user` int unsigned NOT NULL COMMENT '作者',
+                           `content` text NOT NULL COMMENT '内容',
+                           `create_time` datetime NOT NULL COMMENT '创建时间',
+                           `update_time` datetime NOT NULL COMMENT '更新时间',
+                           PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='文章表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `article`
+--
+
+LOCK TABLES `article` WRITE;
+/*!40000 ALTER TABLE `article` DISABLE KEYS */;
+/*!40000 ALTER TABLE `article` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `category` (
+                            `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
+                            `category_name` varchar(32) NOT NULL COMMENT '分类名词',
+                            `category_alias` varchar(32) NOT NULL COMMENT '分类别名',
+                            `create_user` int unsigned NOT NULL COMMENT '创建人ID',
+                            `create_time` datetime NOT NULL COMMENT '创建时间',
+                            `update_time` datetime NOT NULL COMMENT '修改时间',
+                            PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='分类表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+                        `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
+                        `username` varchar(20) NOT NULL COMMENT '用户名',
+                        `password` varchar(32) DEFAULT NULL COMMENT '密码',
+                        `nickname` varchar(10) DEFAULT '' COMMENT '昵称',
+                        `email` varchar(128) DEFAULT '' COMMENT '邮箱',
+                        `user_pic` varchar(128) DEFAULT '' COMMENT '头像',
+                        `create_time` datetime NOT NULL COMMENT '创建时间',
+                        `update_time` datetime NOT NULL COMMENT '修改时间',
+                        PRIMARY KEY (`id`),
+                        UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='用户表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'sfekwpof','e208d1b7379847899e45e046fa0fccbe','','','','2024-03-08 07:38:06','2024-03-08 07:38:06'),(2,'zhengyy','e208d1b7379847899e45e046fa0fccbe','wbc','handsome@yy.com','https://images2.imgbox.com/29/18/CH77MMiB_o.jpg','2024-03-08 07:41:38','2024-03-12 08:48:07'),(3,'erwerw','e208d1b7379847899e45e046fa0fccbe','','','','2024-03-08 07:49:46','2024-03-08 07:49:46'),(4,'testetwt','e208d1b7379847899e45e046fa0fccbe','','','','2024-03-08 15:52:37','2024-03-08 15:52:37');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'big_event'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-03-12 16:54:17
