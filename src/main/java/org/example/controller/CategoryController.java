85a836673cddc524b2a550d71dc9c66383ff41ea
handsomeyyds <handsomeyyds@aliyun.com>
2024-03-12 21:06:18 +0800

    update category

---
+++ src/main/java/org/example/controller/CategoryController.java
@@ -0,0 +1,43 @@
+package org.example.controller;
+
+import org.example.pojo.Category;
+import org.example.pojo.Result;
+import org.example.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+
+    @PostMapping
+    public Result add(@RequestBody @Validated Category category) {
+        categoryService.add(category);
+        return Result.success();
+    }
+
+    @GetMapping
+    public Result<List<Category>> list() {
+        List<Category> cs = categoryService.list();
+        return Result.success(cs);
+    }
+
+    @GetMapping("/detail")
+    public Result<Category> detail(Integer id) {
+        Category category = categoryService.findById(id);
+        return Result.success(category);
+    }
+
+    @PutMapping
+    public Result update(@RequestBody @Validated Category category) {
+        categoryService.update(category);
+        return Result.success();
+    }
+}
--- src/main/java/org/example/pojo/Category.java
+++ src/main/java/org/example/pojo/Category.java
@@ -1,16 +1,24 @@
 package org.example.pojo;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
 public class Category {
+    @NotNull
     private Integer id;
+    @NotEmpty
     private String categoryName;
+    @NotEmpty
     private String categoryAlias;
     private Integer createUser;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createTime;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime updateTime;
 }
